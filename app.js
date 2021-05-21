860f564289f045766099b3eafc5abc805433b468
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-22 01:33:24 +0200

    send a response

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  res.setHeader("Content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Cool beans</title></head>");
+  res.write("<body><h1>Hello</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
