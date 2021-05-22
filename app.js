4031047cc09a89c08c85104a7a8e61af01a4ecde
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-22 02:04:24 +0200

    redirect requests

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const fs = require("fs");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
@@ -11,6 +14,14 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "test");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
   res.setHeader("Content-type", "text/html");
   res.write("<html>");
   res.write("<head><title>Cool beans</title></head>");
